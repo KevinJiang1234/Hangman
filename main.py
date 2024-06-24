32698c61cf4056f17a5f6fee3cd703aa687fb5c4
ericmao1234 <159852120+ericmao1234@users.noreply.github.com>
2024-06-25 11:26:23 +1200

    made intro

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 # Functions
 
 def intro():
+    
+    # Say hello to players
+    print("Hello, player")
+
+
+    # Ask the player their name
+    name = input("what's your name")
+
+
+    # Greet the player and introduce the game
+    print("Welcome to this game,", name)
+    print("This test tests the player's sensitivity to letter and word combinations")
+
+    # Tell players how to play the game
+    print("Our game will give players letters, and players need to look at these letters and input them.")
+    print("If the letters are correct, keep it.")
+    print("If the executioner is wrong, his life will be lost.")
+    print("If you run out of lives, it's game over.")
+
 
 
 
